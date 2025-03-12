50c295bdbdddd028f73c5755a7e884adbaac928c
alanlu <alanlu131089582@gmail.com>
2025-03-12 15:27:38 +0800

    ch4update

---
+++ ch4/PokerGame.java
@@ -0,0 +1,21 @@
+public class PokerGame {
+
+	private DeckOfCards myDeckOfCards;
+
+	public PokerGame() {
+		// TODO
+	}
+
+	public PokerGame(PokerGame game) {
+		// TODO
+	}
+
+	public DeckOfCards getMyDeckOfCards() {
+		return this.myDeckOfCards;
+	}
+	
+	public void dealFiveCards() {
+		// TODO
+	}
+	
+} // end class PokerGame
---
+++ ch4/PokerGameTest.java
@@ -0,0 +1,13 @@
+import java.security.SecureRandom;
+import java.util.Scanner;
+
+public class PokerGameTest {
+
+	public static void main(String[] args) {
+		PokerGame game = new PokerGame();
+		Scanner input = new Scanner(System.in);
+		
+		// TODO (command-line user interface)
+	}
+	
+} // end class PokerGameTest
